e6ed2f0e8fabfbac27430867a1867a59ece38044
neera <kumarmanish150799@gmail.com>
2024-07-05 13:44:59 +0530

    add delet update of product api

--- wasserstoff/BackendTask/Api-Type-Rest/Controllers/productController.js
+++ wasserstoff/BackendTask/Api-Type-Rest/Controllers/productController.js
@@ -1,7 +1,67 @@
+import Product from "../Model/product.js";
 
 
+
+//get all products  =>/rest/api/products
 export const getAllProduct= async (req,res)=>{
+    const products=await Product.find();
+    let productCount=products.length;
     res.status(200).json({
-        message:"All product",
+        productCount,
+        products,
+    })
+}
+
+//create new product => /rest/api/product
+export const newProduct=async (req,res)=>{
+    const product=await Product.create(req.body);
+
+    res.status(200).json({
+        product,
+    })
+}
+
+//get single product details  => /rest/api/product/:id
+export const getProductdetails=async (req,res)=>{
+    const product=await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            error:"product not found",
+        });
+    }
+    res.status(200).json({
+        product,
+    })
+}
+
+//update product details => /rest/api/product/:id
+export const updateProduct=async (req,res)=>{
+    let product=await Product.findById(req?.params?.id);
+    if(!product){
+        return res.status(404).json({
+            error:"product not found",
+        }); 
+    }
+    
+    product=await Product.findByIdAndUpdate(req?.params?.id,req.body,{new :true,});
+
+    res.status(200).json({
+        product,
+    })
+}
+
+//delete product  => /rest/api/product/:id
+export const deleteProduct=async (req,res)=>{
+    const product =await Product.findById(req?.params?.id);
+
+    if(!product){
+        return res.status(404).json({
+            error:"product not found",
+        }); 
+    }
+    await product.deleteOne();
+    res.status(200).json({
+        message:"product deleted successful"
     })
 }
